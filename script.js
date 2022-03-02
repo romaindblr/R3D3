460e5b535146aa965e00c0c4f27a5be9f47934a3
romaindblr <81305762+romaindblr@users.noreply.github.com>
2022-03-02 21:22:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.getElementsById("test").addEventlistener("mouseover", mouseOver);
-document.getElementsById("test").addEventlistener("mouseout", mouseOut);
 
-function mouseOver()
-{
-    document.getElementsById("nom").style.color = "red";
-}
-
-function mouseOut()
-{
-    document.getElementsById("nom").style.color = "black";
-}
