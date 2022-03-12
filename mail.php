99e3cb8f4e640f07136416011690496584c765a3
romaindblr <81305762+romaindblr@users.noreply.github.com>
2022-03-12 14:28:01 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -16,7 +16,7 @@
     </form>
     <?php
     if (isset($_POST['message'])) {
-        $retour = mail('destinataire@free.fr', 'Envoi depuis la page Contact', $_POST['message'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);
+        $retour = mail('romaindv71@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: romaindv71@gmail.com' . "\r\n" . 'Reply-to: ' . $_POST['email']);
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
     }
