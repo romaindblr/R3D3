d2e3d12843dfb6a24621b8226b3f6fd5f7d54f2b
romaindblr <81305762+romaindblr@users.noreply.github.com>
2022-03-12 13:32:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-window.onscroll = function () {
+window.onscroll = function (){
 
-    if (document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.background = "black";
+    if (document.documentElement.scrollTop > 15){
+        document.getElementById("navbar").style.background = "red";
+        document.getElementById("navbar").style.padding = "0px 0px";
     }
-    else {
-        document.getElementById("navbar").style.background = "white";
+    else{
+        document.getElementById("navbar").style.background = "";
+        document.getElementById("navbar").style.padding = "10px 0px";
     }
+}
